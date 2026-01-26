da4160a562b36016732d32e39427873bbc8b94d0
AlfieH6809 <2425244@brunel.ac.uk>
2026-01-26 12:48:48 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# comment
+from operator import truediv
+
+# print("Hello World!")
+#
+# door_open = True
+# name = "Alfie"
+# print(name)
+#
+#
+# price = 20
+# price *= 1.2
+# print(price)
+
+# name = "Alfie"
+# age = 80
+# height = 1.89
+# sentence = "Hello World" + "I am: %s "
+# print("Hello World" + "I am: %s, my age is %d, and I am %f cm tall." %(name, age, height))
+#
+# print("My name is {}, My age is {}, and I am {} cm tall".format(name, age, height))
+#
+# print(name[0])
+# print(name[0:1])
+# print(name[0:2])
+# print(name[0:3])
+# print(name[0:4])
+#
+# print(len(sentence))
+
+
+# name = input("What is your name? ")
+# print("Hello, " + name)
+# age = input("How old are you? ")
+
+
+number = int(input("Give me a big number"))
+final_number = 5+number
+if final_number <= 100:
+    print("That's a small number!")
+elif final_number <= 1000:
+    print("That's not a big number yet!")
+
+
+
+
